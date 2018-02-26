ac325024b050405344bf5df789bcc2080ae56950
Lennart Olsen <lo@cardlay.com>
2018-02-26 17:56:34 +0100

    Initial Commit

---
+++ SensorTagNeckTrainer/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  SensorTagNeckTrainer
+//
+//  Created by Lennart Olsen on 26/02/2018.
+//  Copyright © 2018 lennartolsen.net. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+
+
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(_ aNotification: Notification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
